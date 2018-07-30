f31c5ebc99f15e63a342b614c41cc6af96a87265
jessicazhang601 <jessicazhang601@gmail.com>
2018-07-29 22:19:09 -0400

    not exist, left join, not in

---
+++ CustomerWhoNeverOrder.sql
@@ -0,0 +1,44 @@
+/* 
+Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+Table: Customers.
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Table: Orders.
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Using the above tables as example, return the following:
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+*/
+
+# solution 1
+select Name as "Customers"
+from Customers left join Orders on Customers.ID = Orders.CustomerId
+where Orders.CustomerId is null
+
+# solution 2
+SELECT A.Name from Customers A
+WHERE NOT EXISTS (SELECT 1 FROM Orders B WHERE A.Id = B.CustomerId)
+
+# solution 3
+
+SELECT A.Name from Customers A
+WHERE A.Id NOT IN (SELECT B.CustomerId from Orders B)
